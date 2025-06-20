be9d92cac856c8e69437d8ddbda72b02d4480e4c
R-Tedy <stedyroland2@gmail.com>
2025-06-21 00:35:52 +0300

    implement companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,53 @@
+import CompanionCard from '@/components/CompanionCard'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
+const companions = [
+  {
+    id: '973829',
+    name: 'Langy the Modern Speaker',
+    topic: 'An introduction to languate',
+    subject: 'Language',
+    duration: 45,
+    color: '#E5D0FF',
+    bookmarked: false,
+  },
+  {
+    id: '234173',
+    name: 'Mathsy the Number Wizard',
+    topic: 'Addition and Simple Arithmetic',
+    subject: 'Mathematics',
+    duration: 45,
+    color: '#BDE7FF',
+    bookmarked: false,
+  },
+  {
+    id: '172493',
+    name: 'Babayo the Biological Baba',
+    topic: 'An introduction to Human Anatomy',
+    subject: 'Biology',
+    duration: 45,
+    color: '#FFDA6E',
+    bookmarked: false,
+  },
+]
+
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline text-center'>This is the best way to start learning</h1>
-      <Button>This is a button</Button>
-    </div>
+    <main>
+      <h1>
+        Popular Companions
+      </h1>
+
+      <section className='home-section'>
+        {companions.map((companion)=>(
+          <CompanionCard 
+            key={companion.id} 
+            {...companion}
+          />
+        ))}
+      </section>
+    </main>
   )
 }
 
